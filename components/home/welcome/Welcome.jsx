e3ca6c680a33715c0be8b86a0c2c4925aa7c0443
Luis-eduardo-sl <luis.lucena@aluno.ifsp.edu.br>
2024-05-14 22:19:08 -0300

    init style home

--- components/home/welcome/Welcome.jsx
+++ components/home/welcome/Welcome.jsx
@@ -1,12 +1,48 @@
-import React from 'react'
-import { View, Text } from 'react-native'
+import {useState} from 'react'
+import {
+   View, 
+   Text,
+   TextInput,
+   TouchableOpacity,
+   Image,
+   FlatList
+} from 'react-native'
+import { useRouter } from 'expo-router'
 
 import styles from './welcome.style'
+import { icons, SIZES } from '../../../constants'
 
 const Welcome = () => {
+  const router = useRouter();
+
+
   return (
     <View>
-      <Text>Welcome</Text>
+      <View style={styles.container}>
+        <Text style={styles.UserName}>Olá Luis</Text>
+        <Text style={styles.welcomeMessage}>Ache o emprego perfeito</Text>
+      </View>
+
+      <View style={styles.searchContainer}>
+        <View style={styles.searchWrapper}>
+          <TextInput 
+            style={styles.searchInput}
+            value=''
+            onChange={() => {}}
+            placeholder='Oque está procurando?'
+          />
+        </View>
+
+        <TouchableOpacity style={styles.searchBtn} onPress={() => {}}>
+          <Image 
+            source={icons.search}
+            resizeMode='contain'
+            style={styles.searchBtnImage}
+          />
+        </TouchableOpacity>
+      </View>
+
+      {/* min 43 */}
     </View>
   )
 }
